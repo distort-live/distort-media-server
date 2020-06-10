98c099dbb6fe0f3bcd3bffb07a94c9f07b5a9fa1
MagicFun1241 <nganzikov@gmail.com>
2020-06-10 11:44:20 +0400

    Start migration to TypeScript

---
+++ src/node_media_server.js
@@ -0,0 +1,75 @@
+//
+//  Created by Mingliang Chen on 17/8/1.
+//  illuspas[a]gmail.com
+//  Copyright (c) 2018 Nodemedia. All rights reserved.
+//
+
+const Logger = require('./core/logger');
+const NodeRtmpServer = require('./node_rtmp_server');
+const NodeHttpServer = require('./httpServer');
+const NodeTransServer = require('./node_trans_server');
+const NodeRelayServer = require('./node_relay_server');
+const context = require('./core/context');
+const Package = require("../package.json");
+
+export default class NodeMediaServer {
+  constructor(config) {
+    this.config = config;
+  }
+
+  run() {
+    Logger.setLogType(this.config.logType);
+    Logger.log(`Node Media Server v${Package.version}`);
+    if (this.config.rtmp) {
+      this.nrs = new NodeRtmpServer(this.config);
+      this.nrs.run();
+    }
+
+    if (this.config.http) {
+      this.nhs = new NodeHttpServer(this.config);
+      this.nhs.run();
+    }
+
+    if (this.config.trans) {
+      if (this.config.cluster) {
+        Logger.log('NodeTransServer does not work in cluster mode');
+      } else {
+        this.nts = new NodeTransServer(this.config);
+        this.nts.run();
+      }
+    }
+
+    if (this.config.relay) {
+      if (this.config.cluster) {
+        Logger.log('NodeRelayServer does not work in cluster mode');
+      } else {
+        this.nls = new NodeRelayServer(this.config);
+        this.nls.run();
+      }
+    }
+
+    process.on('uncaughtException', function (err) {
+      Logger.error('uncaughtException', err);
+    });
+  }
+
+  on(eventName, listener) {
+    context.nodeEvent.on(eventName, listener);
+  }
+
+  stop() {
+    if (this.nrs) {
+      this.nrs.stop();
+    }
+    if (this.nhs) {
+      this.nhs.stop();
+    }
+    if (this.nls) {
+      this.nls.stop();
+    }
+  }
+
+  getSession(id) {
+    return context.sessions.get(id);
+  }
+}
