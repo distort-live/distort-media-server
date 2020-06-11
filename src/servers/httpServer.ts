58c81af0b1a658ec7085472b6847f37ed681cb0d
MagicFun1241 <nganzikov@gmail.com>
2020-06-12 00:52:40 +0400

    Hot fix

--- src/servers/httpServer.ts
+++ src/servers/httpServer.ts
@@ -45,7 +45,7 @@ export default class HttpServer {
         app.use(bodyParser.urlencoded({extended: true}));
 
         app.all('*', (req, res, next) => {
-            res.header("Access-Control-Allow-Origin", config.http.allow_origin);
+            res.header("Access-Control-Allow-Origin", config.http.allow_origin + "");
             res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
             res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
             res.header("Access-Control-Allow-Credentials", "true");
